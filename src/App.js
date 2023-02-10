24eba5f3f85649986fd0fb2533f33f4a687299c5
snnbotchway <solomonbotchway7@gmail.com>
2023-02-10 20:32:15 +0000

    Add routing for the components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Welcome from "./components/Welcome";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+
+import Layout from "./components/Layout";
+// import store from "./store";
+
+import Home from "./components/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<BrowserRouter>
+				<Layout>
+					<Routes>
+						<Route exact path="/" element={<Welcome />} />
+						<Route exact path="/home" element={<Home />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/signup" element={<Signup />} />
+					</Routes>
+				</Layout>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
